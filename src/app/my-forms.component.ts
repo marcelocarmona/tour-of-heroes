f7bbb1cb10b4089ddfca3dea0212577443d5edac
marcelocarmona <marcelo@marcelocarmona.com>
2017-03-14 17:55:56 -0300

    reactiveForms formArray 🖖

--- src/app/my-forms.component.ts
+++ src/app/my-forms.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+class Address {
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+}
 
 @Component({
   moduleId: module.id,
   templateUrl: `./my-forms.component.html`,
-  styleUrls: ['./my-forms.component.css']
+  styleUrls: ['./my-forms.component.css'],
 })
-export class MyFormsComponent {
+export class MyFormsComponent implements OnInit {
+
+
   heroForm: FormGroup; // <--- heroForm is of type FormGroup
+  states:string[] = ['CA', 'MD', 'OH', 'VA'];
 
   constructor(private fb: FormBuilder) { // <--- inject FormBuilder
     this.createForm();
+  }
+
+  ngOnInit(): void {
+    this.heroForm.reset({
+      name: ''
+    });
+    this.setAddresses([{
+      street: 'calle',
+      city: 'ciudad',
+      state: 'state',
+      zip: 'zip'
+    }]);
+  }
+
+  ngOnChanges(): void {
+
   }
 
   createForm() {
@@ -18,8 +44,37 @@ export class MyFormsComponent {
       name: ['', [ Validators.required,
                    Validators.minLength(3),
                    Validators.maxLength(6)] ], // <--- the FormControl called "name"
-      street: ['', [ Validators.required ]],
-      city: ['', [ Validators.required ]]
+      secretLairs: this.fb.array([]),
+      power: '',
+      sidekick: ''
     });
+  }
+
+  setAddresses(addresses: Address[]) {
+    const addressFGs = addresses.map(address => this.fb.group(address));
+    const addressFormArray = this.fb.array(addressFGs);
+    this.heroForm.setControl('secretLairs', addressFormArray);
+  }
+
+  addAddress() {
+    console.log('agregar address');
+    this.addLair()
+  }
+
+  get secretLairs(): FormArray {
+    return this.heroForm.get('secretLairs') as FormArray;
+  };
+
+  addLair() {
+    this.secretLairs.push(this.fb.group({
+      street: 'calle',
+      city: 'ciudad',
+      state: 'state',
+      zip: 'zip'
+    }));
+  }
+
+  removeAddress() {
+    this.secretLairs.removeAt(this.secretLairs.length - 1)
   }
 }
